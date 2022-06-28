0a2d98e9a40e9f835a6c0c7a9d56cf440d056cb1
Jair Hernández <jair_097@hotmail.com>
2022-06-28 15:49:27 -0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Paths from './Paths'
@@ -6,16 +5,19 @@ import './styles/index.css'
 import { LoginProvider } from './context/LoginContext'
 import { ProductsProvider } from './context/ProductsContext'
 import Header from './components/Header'
+import { Suspense, StrictMode } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LoginProvider>
-      <BrowserRouter>
-        <ProductsProvider>
-          <Header />
-          <Paths />
-        </ProductsProvider>
-      </BrowserRouter>
-    </LoginProvider>
-  </React.StrictMode>
+  <StrictMode>
+    <Suspense>
+      <LoginProvider>
+        <BrowserRouter>
+          <ProductsProvider>
+            <Header />
+            <Paths />
+          </ProductsProvider>
+        </BrowserRouter>
+      </LoginProvider>
+    </Suspense>
+  </StrictMode>
 )
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import useAPIProducts from '../hooks/useAPIProducts'
 
 const Product = () => {
+  const { id = '' } = useParams()
+  const {
+    data: product,
+    error
+  } = useAPIProducts(`https://ecomerce-master.herokuapp.com/api/v1/item/${id}`)
+  if (error) return <p>{error}</p>
+
   return (
-    <div>Product</div>
+    <article>
+      <p>{product.product_name}</p>
+      <img loading='lazy' src={product.image} alt={product.product_name} />
+    </article>
   )
 }
-
 export default Product

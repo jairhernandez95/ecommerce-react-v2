d56eba70e6cfa39a86e7914b4822a1f5caeb8476
Jair Hernández <jair_097@hotmail.com>
2022-06-30 19:29:18 -0700

    update

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,10 +1,27 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import apiGetUser from '../services/apiGetUser'
 
 export const UserContext = createContext(null)
 
 export const UserProvider = ({ children }) => {
-  const [userName, setUserName] = useState([])
-  const [userRole, setUserRole] = useState([])
+  const [userName, setUserName] = useState('')
+  const [userRole, setUserRole] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await apiGetUser()
+      const name = await response.data.user.first_name
+      setUserName(name)
+      console.log(userName)
+      const role = await response.data.role
+      setUserRole(role)
+      console.log(userRole)
+    }
+    if (window.localStorage.getItem('token')) {
+      fetchData()
+    }
+  }, [])
+
   const initialValue = {
     userName,
     setUserName,
